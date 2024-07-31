2c00f2d019aa9377a887ace7e51f1d965d90ac6d
TomKe <110755646+TomKe123@users.noreply.github.com>
2024-07-31 21:17:01 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -46,10 +46,10 @@ export default {
     let titleTime
     document.addEventListener('visibilitychange', function () {
       if (document.hidden) {
-        document.title = '死鬼去哪里了！'
+        document.title = '诶~ 别走啊！'
         clearTimeout(titleTime)
       } else {
-        document.title = '(つェ⊂)咦!又好了!'
+        document.title = '总算回来了'
         titleTime = setTimeout(function () {
           document.title = OriginTitile
         }, 2000)
